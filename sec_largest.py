c976baa4e4595766f043cecf4c1f735a5357e58f
Aryan Sai <aryansaiarvapelly@gmail.com>
2023-07-16 14:51:52 +0530

    sec largest

--- sec_largest.py
+++ sec_largest.py
@@ -1,21 +1,23 @@
 import random
 
-def find_largest(a):
-    if len(a) == 0:
-        return float("-inf")
-    elif len(a) == 1:
-        return a[0]
+
+def find_second_largest(a):
+    if len(a) == 1:
+        return a[0], float("-inf")
     else:
-        print(a[: len(a) / 2])
-        max1 = find_largest(a[: len(a) / 2])
-        print(a[len(a) / 2 :])
-        max2 = find_largest(a[len(a) / 2 :])
+        max1, sec1 = find_second_largest(a[: len(a) // 2])
+        max2, sec2 = find_second_largest(a[len(a) // 2 :])
+        maxi = max(max1, max2)
         if max1 > max2:
-            return max1
+            second_max = max(sec1, max2)
         else:
-            return max2
+            second_max = max(sec2, max1)
+        return maxi, second_max
 
 
+a = [5, 2, 8, 10, 1, 6, 12, 13]
 a = random.sample(range(0, 100), 5)
 print(a)
-print("\nThe largest element is {}!".format(find_largest(a)))
+largest, second_largest = find_second_largest(a)
+print("The largest element is {}!".format(largest))
+print("The second largest element is {}!".format(second_largest))
