8bc305078c4807fd58170408e6f804facf3fd473
Aryan Sai <aryansaiarvapelly@gmail.com>
2023-07-20 11:00:01 +0530

    bruteforce second largest

--- bruteforce_sec_largest.py
+++ bruteforce_sec_largest.py
@@ -1,7 +1,22 @@
 import random
 
-a = random.sample(range(0, 10000), 1000)
+a = random.sample(range(0, 10000), 500)
 print("The list of elements: ", a)
-negative_infinity = float("-inf")
-curr_largest = negative_infinity
+
+# true largest and sec_largest
+# b = list(set(a))
+# b.sort()
+# print(max(a), b[-2])
+
+largest = 0
+sec_largest = float("-inf")
+
 i = 0
+while i < len(a):
+    if a[i] > largest:
+        sec_largest = largest
+        largest = a[i]
+    elif a[i] > sec_largest and a[i] < largest:
+        sec_largest = a[i]
+    i = i + 1
+print(largest, sec_largest)
