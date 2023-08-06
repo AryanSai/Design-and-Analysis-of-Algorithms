2c44b7f8ba88adc15dd3842744cedb12654cdbd6
Aryan Sai <aryansaiarvapelly@gmail.com>
2023-08-06 11:30:49 +0530

    insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,13 +1,19 @@
-# import random
+import random
 
 
-# def insertion_sort(a):
-#     i = 1
-#     n = len(a)
-#     while i != n:
-       
-# def main():
-#     a = [12, 11, 13, 5, 6]
-#     # a = random.sample(range(0, 10000), 500)
-#     print("The list of elements: ", a)
-#     insertion_sort(a)
+def insertion_sort(a):
+    n = len(a)
+    for j in range(1, n):
+        key = a[j]
+        i = j - 1
+        while i >= 0 and a[i] > key:
+            a[i + 1] = a[i]
+            i = i - 1
+        a[i + 1] = key
+    return a
+
+
+# a = [12, 11, 13, 5, 6]
+a = random.sample(range(0, 100), 50)
+print("The list of elements: {}".format(a))
+print("The sorted list of elements: {}".format(insertion_sort(a)))
