26becadd27b167c97132b289ed45bf6601c2b20b
Aryan Sai <aryansaiarvapelly@gmail.com>
2023-08-25 14:21:38 +0530

    kmp

--- String Matching/KMP.py
+++ String Matching/KMP.py
@@ -1,17 +1,44 @@
-# find the next of the pattern
-def next(pattern):
+def compute_next(pattern):
     next = [0] * len(pattern)
+    j = 0  # length of prefix that is also suffix for substr ending at i
     for i in range(1, len(pattern)):
-        x = next[i - 1]
-        while pattern[i] != pattern[x]:
-            if x == 0:
-                x = -1
-                break
-            x = next[x - 1]
-        next[i] = x + 1
+        while (
+            j > 0 and pattern[i] != pattern[j]
+        ):  # we need to find a shorter prefix that is also a suffix
+            j = next[j - 1]
+        if (
+            pattern[i] == pattern[j]
+        ):  # we extended current proper prefix and can increment the length of lps
+            j += 1
+        next[i] = j  # updating the lps of substr ending at i
     return next
 
 
-pattern = "aabaaba"
-print(next(pattern))
-# kmp algorithm
+def KMPSearch(text, pattern):
+    n = len(text) 
+    m = len(pattern)
+    if m > n:
+        print("Pattern String bigger than the Text String!!")
+    else:
+        next = compute_next(pattern)
+        print("The next array of the pattern is: {}".format(next))
+        i = 0  # Index for the text
+        j = 0  # Index for the pattern
+        while i < n:
+            if pattern[j] == text[i]: # if characters match, move both indices forward
+                i += 1
+                j += 1
+                if j == m:
+                    print("Found pattern at index {}".format(str(i - j)))
+                    j = next[j - 1] # Update j using the "next" array to continue searching
+            else:
+                if j != 0: # Characters don't match
+                    j = next[j - 1] 
+                else:  # No useful information in "next," move i forward
+                    i += 1
+
+
+text = "abcabcaabbabc"
+pattern = "abc"
+# kmp(text, pattern)
+KMPSearch(text, pattern)
